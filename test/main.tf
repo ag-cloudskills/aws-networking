f8966e8007b8cbc7af6be46fcd56fad338685692
Ashish <84917926+ag-cloudskills@users.noreply.github.com>
2024-07-11 15:40:52 +1000

    1 setup environment (#2)
    * #1 bash scripts setup done
    
    * #1 fixed tf install
    
    * #1 fixing the tf setup
    
    * msg
    
    * bash scripts fixed
    
    * #1 tasks completed

---
+++ test/main.tf
@@ -0,0 +1,52 @@
+terraform {
+  cloud {
+    organization = "learnterraformacasual"
+
+    workspaces {
+      name = "aws-networking"
+    }
+  }
+}
+
+
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.6.2"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.57.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  # Configuration options
+  region                  = "ap-southeast-2"
+}
+
+
+
+provider "random" {
+  # Configuration options
+}
+
+
+
+resource "random_string" "bucket_name" {
+  length           = 16
+  special          = false
+  upper            = false
+  lower            = true   
+}
+
+resource "aws_s3_bucket" "example" {
+ bucket = random_string.bucket_name.result
+}
+
+output "s3_bucket_name"{
+ value = aws_s3_bucket.example.id
+}
